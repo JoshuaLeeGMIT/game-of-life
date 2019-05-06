6c428b1e53e9f240ed61604265ed0de079c493b3
Joshua Lee <lee.son.wai@gmail.com>
2019-05-06 15:24:47 +0100

    shop: Misc cleanups

--- shop.js
+++ shop.js
@@ -1,9 +1,6 @@
-let value;
-
-/*Reapeat line 8 for ALL peices and change the 4 buttons into 1 buttons*/
-
 function addToCart() {
 	let cart = JSON.parse(localStorage.getItem('cart'));
+
 	if(sessionStorage.loggedIn == 0)
 	{
 		alert("please sign in");
@@ -20,27 +17,8 @@ function addToCart() {
 }
 
 function display() {
-		document.getElementById("gDes").innerHTML = "A basic piece which perform a simple task. It travels in a diagonal direction. €1.00 a piece";
-		
-		document.getElementById("lDes").innerHTML = "A unique piece which perform a simple task. It travels in a straight direction. €2.50 a piece";
-		
-		document.getElementById("pDes").innerHTML = "A basic enough piece which perform a task for infinity. It can be broken by other piece upon collision. €5.99 a piece";
-		
-		document.getElementById("eDes").innerHTML = "A simple piece which perform a complex task for infinity. It can be broken by other piece upon collision. €6.99 a piece";
+	document.getElementById("gDes").innerHTML = "A basic piece which perform a simple task. It travels in a diagonal direction. €1.00 a piece";
+	document.getElementById("lDes").innerHTML = "A unique piece which perform a simple task. It travels in a straight direction. €2.50 a piece";
+	document.getElementById("pDes").innerHTML = "A basic enough piece which perform a task for infinity. It can be broken by other piece upon collision. €5.99 a piece";
+	document.getElementById("eDes").innerHTML = "A simple piece which perform a complex task for infinity. It can be broken by other piece upon collision. €6.99 a piece";
 }
-/*
-function addLwss() {	
-	lwss.quantity = Number(document.getElementById("lwssInput").value);
-	localStorage.setItem ('lwss', JSON.stringify(lwss));
-}
-
-function addPenta() {
-	penta.quantity = Number(document.getElementById("pentaInput").value);
-	localStorage.setItem ('penta', JSON.stringify(penta));
-}
-
-function addExpl() {
-	expl.quantity = Number(document.getElementById("explInput").value);
-	localStorage.setItem ('expl', JSON.stringify(expl));
-}
-*/

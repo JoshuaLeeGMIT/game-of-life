2715ac629aab6ecc50c42b895953d68f2319acf3
Eoin-Lernihan <48314832+Eoin-Lernihan@users.noreply.github.com>
2019-05-06 13:42:02 +0100

    Add files via upload

--- shop.js
+++ shop.js
@@ -1,34 +1,34 @@
-let glider = {
-	'name': "glider",
-	'price': 1.00,
-	'quantity': 0
-};
+let value;
 
-let lwss = {
-	'name': "lwss",
-	'price': 2.50,
-	'quantity': 0
-};
+/*Reapeat line 8 for ALL peices and change the 4 buttons into 1 buttons*/
 
-let penta = {
-	'name': "penta",
-	'price': 5.99,
-	'quantity': 0
-};
-
-let expl = {
-	'name': "expl",
-	'price': 6.99,
-	'quantity': 0
-};
-
-let test;
-
-function addGlider() {	
-	glider.quantity = Number(document.getElementById("gliderInput").value);
-	localStorage.setItem ('glider', JSON.stringify(glider));
+function addToCart() {
+	let cart = JSON.parse(localStorage.getItem('cart'));
+	if(sessionStorage.loggedIn == 0)
+	{
+		alert("please sign in");
+	}	
+	else
+	{
+		alert("Successfuly added to cart");
+		cart.nGliders += Number(document.getElementById("gliderInput").value);
+		cart.nLwss +=  Number(document.getElementById("lwssInput").value);
+		cart.nPenta +=  Number(document.getElementById("pentaInput").value);
+		cart.nExpl +=  Number(document.getElementById("explInput").value);
+		localStorage.cart = JSON.stringify(cart);
+	}
 }
 
+function display() {
+		document.getElementById("gDes").innerHTML = "A basic piece which perform a simple task. It travels in a diagonal direction. €1.00 a piece";
+		
+		document.getElementById("lDes").innerHTML = "A unique piece which perform a simple task. It travels in a straight direction. €2.50 a piece";
+		
+		document.getElementById("pDes").innerHTML = "A basic enough piece which perform a task for infinity. It can be broken by other piece upon collision. €5.99 a piece";
+		
+		document.getElementById("eDes").innerHTML = "A simple piece which perform a complex task for infinity. It can be broken by other piece upon collision. €6.99 a piece";
+}
+/*
 function addLwss() {	
 	lwss.quantity = Number(document.getElementById("lwssInput").value);
 	localStorage.setItem ('lwss', JSON.stringify(lwss));
@@ -43,3 +43,4 @@ function addExpl() {
 	expl.quantity = Number(document.getElementById("explInput").value);
 	localStorage.setItem ('expl', JSON.stringify(expl));
 }
+*/
